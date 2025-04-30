6bf0ecc15a3333661260742ce3fc5b7504a42a90
Codearlo <flavio.cabanah@gmail.com>
2025-04-30 11:32:27 -0500

    a

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -10,7 +10,7 @@ $userName = $isLoggedIn ? $_SESSION['user_name'] : '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MisterJugo - Jugos Naturales</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
