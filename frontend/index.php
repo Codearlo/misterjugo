e3212eb5ecfce4d43d235bdd1b556167038838e9
Codearlo <flavio.cabanah@gmail.com>
2025-05-17 17:43:08 -0500

    a

--- frontend/index.php
+++ frontend/index.php
@@ -26,12 +26,12 @@
     <div class="container">
         <div class="hero-content">
             <div class="hero-image">
-                <img src="./images/jugos-misterjugo.jpg" alt="Logo Mister Jugo">
+                <img src="./images/banner.jpg" alt="Logo Mister Jugo">
             </div>
             <div class="hero-text">
                 <h1>¿Ya comiste? ¿Qué esperas para ordenar?</h1>
                 <p>Descubre nuestros jugos y sándwich</p>
-                <a href="productos.php" class="btn-order-now">Ordena Ya!!</a>
+                <a href="productos" class="btn-order-now">Ordena Ya!!</a>
             </div>
         </div>
     </div>
