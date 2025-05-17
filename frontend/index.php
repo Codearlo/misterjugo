2a70696217ef922e8a7bfee149290423736564c5
Codearlo <flavio.cabanah@gmail.com>
2025-05-17 17:22:22 -0500

    a

--- frontend/index.php
+++ frontend/index.php
@@ -1,5 +1,6 @@
 <?php include 'includes/header.php'; ?>
-<link rel="stylesheet" href="./css/index.css"> <!-- Estilo principal -->
+<link rel="stylesheet" href="./css/index.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins :wght@400;500;600;700&display=swap" rel="stylesheet"> <!-- Estilo principal -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css "> <!-- Iconos -->
 
 <script>
