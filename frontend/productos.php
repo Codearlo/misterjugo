1659f0f55b22b5a0c2b93933b19945c02054047a
Codearlo <flavio.cabanah@gmail.com>
2025-05-18 23:16:49 -0500

    a

--- frontend/productos.php
+++ frontend/productos.php
@@ -68,6 +68,7 @@ if ($result_productos && $result_productos->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productos - MisterJugo</title>
+    <script src="js/cache_control.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/productos.css">
